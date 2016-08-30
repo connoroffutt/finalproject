464c5413249a4ce45d13c2a40f790b8502a9425b
Connor Offutt <ConnorOffutt@Connor-Offutts-MacBook-Pro.local>
2016-08-29 20:15:28 -0400

    final-project-draft1

---
+++ final-project02/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $('.main-burger').on('click', function() {
+    $('.main-burger').toggleClass('is-active')
+    $('.panel').toggleClass('slide-out');
+
+
+  });
+
+
+});
